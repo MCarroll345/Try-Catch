946bcc4a2d901bed3ae8f28f21ab373f336a6b5b
Mark Carroll <g00419267@atu.ie>
2023-11-14 13:48:05 +0000

    While loop for made

--- src/main/java/ie/atu/TryCatch.java
+++ src/main/java/ie/atu/TryCatch.java
@@ -4,25 +4,29 @@ import java.util.Scanner;
 
 public class TryCatch {
     public static void main(String[] args) {
-        try{
-            System.out.println("Please enter a number between 0 - 9");
-            Scanner myscan = new Scanner(System.in);
+
+        while(true) {
+            try {
+                System.out.println("Please enter a number between 0 - 9");
+                Scanner myscan = new Scanner(System.in);
 
                 String input = myscan.nextLine();
 
                 int myNum = Integer.parseInt(input);
 
 
-                if (myNum >= 0 && myNum <= 9){
-                    System.out.println("You entered" + myNum);
+                if (myNum >= 0 && myNum <= 9) {
+                    System.out.println("You entered " + myNum);
+                    break;
                 }
                 else {
                     System.out.println("Not a valid munber");
                 }
             }
-            catch(NumberFormatException e){
-                System.out.println("Wrong");
+            catch (NumberFormatException e) {
+                System.out.println("Number not entered");
             }
+        }
 
     }
 }
